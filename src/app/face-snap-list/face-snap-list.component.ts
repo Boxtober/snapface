52436d656a45504c773d72db603b2fb8afd6e6fd
boxtober <contact.asartwork@gmail.com>
2023-08-29 11:46:55 +0200

    :building_construction: Feat : Form

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { Subject, interval, tap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit {
+
+export class FaceSnapListComponent implements OnInit, OnDestroy {
 
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsService: FaceSnapsService) { }
-
-  ngOnInit(): void {
-    this.faceSnaps =  this.faceSnapsService.faceSnaps;
   
+  ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
+    this.faceSnaps =  this.faceSnapsService.getAllFaceSnaps();
+    interval(1000).pipe(
+    tap(console.log),
+    takeUntil(this.destroy$)
+  ).subscribe();
+  
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
